bc372743abec9744f43cd8790e0d766842b0a02f
Diego16061989 <diegomonteiro6437@gmail.com>
2024-10-18 17:33:45 -0300

    py

---
+++ main.js
@@ -0,0 +1,30 @@
+let num = 0
+let yep = 0
+
+function mudar(){
+    num++
+    if (num == 1){
+        document.body.style.transition = "3s"
+        document.body.style.backgroundImage = "url(../images/background.png)"
+    }
+    if (num == 2){
+        num = 0
+    }
+    if (num == 0){
+        document.body.style.backgroundImage = ""
+    }
+}
+
+function appear(){
+    yep++
+    if (yep == 1){
+        document.getElementById("barra-config").style.display = "inline-flex"
+        document.getElementById("barra-config").style.transition = "1s"
+    }
+    if (yep == 2){
+        yep = 0
+    }
+    if (yep == 0){
+        document.getElementById("barra-config").style.display = "none"
+    }
+}
